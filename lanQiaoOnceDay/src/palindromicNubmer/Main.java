0c263ae2e5e7abadef8d42e0dd050831283201d5
Awacky <awacky@163.com>
2020-01-02 20:37:02 +0800

    每日一刷

---
+++ lanQiaoOnceDay/src/palindromicNubmer/Main.java
@@ -0,0 +1,19 @@
+package palindromicNubmer;
+
+/**
+ * @author Awacky
+ * @date 2020/1/2 20:36
+ */
+public class Main {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        for (int i = 1; i <10 ; i++) {
+            for(int j =0;j<10;j++){
+                System.out.println(i*1000+j*100+j*10+i);
+            }
+        }
+
+
+    }
+
+}
