40ded82e4c1c470be3d17c4a550ee04d119ae9e1
Awacky <awacky@163.com>
2020-02-01 22:44:33 +0800

    整理

--- lanQiaoOnceDay/src/lQofficeWebsite/_20_1_31_insertSort.java
+++ lanQiaoOnceDay/src/lQofficeWebsite/_20_1_31_insertSort.java
@@ -8,7 +8,7 @@ package lQofficeWebsite;
 public class _20_1_31_insertSort {
     public static  void main(String[] args) {
         int[] data = {3,9,6,1,2};
-//        insertionSort(data);
+        insertionSort(data);
 
     }
 
@@ -19,17 +19,17 @@ public class _20_1_31_insertSort {
 //            while (position > 0&&data[position-1].c)
 //        }
 //    }
-    public static <T extends Comparable<? super  T>>void insertionSort(T[] data){
+    public static void insertionSort(int[] data){
         for (int index = 1;index < data.length; index++){
-            T key = data[index];
+            int key = data[index];
             int position = index;
-            while (position > 0 && data[position-1].compareTo(key) > 0){
+            while (position > 0 && (data[position-1]-key) > 0){
                 data[position]  = data[position-1];
                 position--;
             }
             data[position] = key;
         }
-        for (T value : data
+        for (int value : data
              ) {
             System.out.println(value);
 
