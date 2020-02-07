e1ef98cc9bb1b8089d2ed121d96e2fc063e8c997
Awacky <awacky@163.com>
2020-02-07 21:27:31 +0800

    小练习

---
+++ lanQiaoOnceDay/src/lQofficeWebsite/practice/MinFreeId.java
@@ -0,0 +1,20 @@
+package lQofficeWebsite.practice;
+
+/**
+ * @author Awacky
+ * @date 2020/2/7 16:21
+ */
+////寻找自由数
+//public class MinFreeId {
+//    public static int find4(int[] arr,int l,int r){
+//        if(l > r){
+//            return l+1;
+//        }
+//        int midIndex = l + ((r - l) >> 1); //中间下标
+//        int q = SelectK.selectK(arr,l,r,midIndex-1);
+//        int t = midIndex + 1;
+//        if (q == t){
+//            return MinFreeId.find4(arr,midIndex+1,r);
+//        }
+//    }
+//}
