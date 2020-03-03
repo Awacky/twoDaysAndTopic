45373b6c6f5f2ef33ed2ac7a2d4fd08df2c70b7f
Awacky <awacky@163.com>
2020-03-03 23:29:53 +0800

    添加了题目

--- lanQiao/src/main/java/org/lanqiao/algo/elementary/_07dfs/CC9_4非空子集.java
+++ lanQiao/src/main/java/org/lanqiao/algo/elementary/_07dfs/CC9_4非空子集.java
@@ -1,11 +1,25 @@
 package org.lanqiao.algo.elementary._07dfs;
 
+import java.util.Set;
+
 /**
  * @author Awacky
  * @date 2020/3/2 23:33
  */
 /*题目描述
-* 编写一个方法，返回某集合的所有子集*/
+* 编写一个方法，返回某集合的所有子集
+* 给定一个int数组A和数组大小int n，请返回A的所有非空子集
+* 保证A的元素个数小于等于20，且元素互异
+*
+* 个子集内部从大到小排序，子集之间字典逆序排序*/
 public class CC9_4非空子集 {
-
+//    迭代法
+    /**
+     * @Description: 迭代法
+     * @Param: n
+     * @return: Set<String>
+     */
+    public Set<String> getList(int n){
+        
+    }
 }
