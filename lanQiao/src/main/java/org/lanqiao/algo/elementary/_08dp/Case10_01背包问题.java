aad4d053c777dacdc6bfd977ff035f037aa00b28
Awacky <awacky@163.com>
2020-04-10 23:09:41 +0800

    dp开始入门学习

---
+++ lanQiao/src/main/java/org/lanqiao/algo/elementary/_08dp/Case10_01背包问题.java
@@ -0,0 +1,20 @@
+package org.lanqiao.algo.elementary._08dp;
+
+/**
+ * @author Awacky
+ * @date 2020/4/10 23:06
+ */
+/*
+* 有n个重量和价值分别为wi，vi的物品，从这些物品中挑选出总重量不超过W的物品，求所有挑选方案中价值总和的最大值
+*
+* 输入：
+* n = 4
+* (w,v) = {(2,3),(1,2),(3,4),(2,2)}
+*
+* 输出：
+* 7选择第0,1,3号物品*/
+public class Case10_01背包问题 {
+    public static void main(String[] args) {
+
+    }
+}
