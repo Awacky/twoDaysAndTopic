13c1c65fb21cbbde23001dfd1d520bd062cf0b64
awacky <awacky@163.com>
2020-09-22 22:01:41 +0800

    beikaolanqiao

---
+++ lanQiaoOnceDay/src/ahaAlgorithm/secondChapter/XiaoMaoDiaoYu.java
@@ -0,0 +1,46 @@
+package ahaAlgorithm.secondChapter;
+
+/**
+ * @Author Awacky
+ * Date on 2020/9/22  下午9:34
+ */
+public class XiaoMaoDiaoYu {
+////    实现队列,先进先出原则
+//    class QueueSequence{
+//        private String[] arr;//队列数组
+//        int tail = 0;   //队尾部实现
+//
+////        向队列添加元素
+//    public void  push(String[] arr,String value){
+//        if (tail < arr.length){
+//            arr[tail] = value;
+//            tail++;
+//            return;
+//        }else {
+//            System.out.println("队列已经满了");
+//            return;
+//        }
+//    }
+//
+////    取出队列元素
+//    public String pop(String[] arr){
+//        if (arr[0] == null){
+//            System.out.println("队列为空");
+//            return null;
+//        }else {
+//            String rs = arr[0];
+//
+//        }
+//    }
+//
+////    队列的元素向前移动
+//    public void move(String[] arr){
+//        for (int i = 0;i < arr.length;i++){
+//            if (arr)
+//        }
+//    }
+//}
+public static void main(String[] args) {
+
+}
+}
